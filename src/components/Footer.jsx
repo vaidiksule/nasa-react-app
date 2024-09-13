bf88435629ddb373e4c8ffa7765f5932e7e3d0e3
vaidiksule <144515061+vaidiksule@users.noreply.github.com>
2024-09-13 21:50:00 +0530

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,7 +4,7 @@ export default function Footer(props) {
     <footer>
         <div className="bgGradient"></div>
         <div>
-            <h1>APOO Project</h1>
+            <h1>APOD Project</h1>
             <h2>{data?.title}</h2>
         </div>
         <button onClick={handleToggleModal}>
