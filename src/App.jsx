8c396d2542795958ed8140d850ead6cf60075dd9
vaidiksule <144515061+vaidiksule@users.noreply.github.com>
2024-09-13 21:40:38 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import SideBar from "./components/SideBar"
-import Main from "./components/main"
+import Main from "./components/Main"
 import Footer from "./components/Footer"
 import { useEffect, useState } from "react"
 
